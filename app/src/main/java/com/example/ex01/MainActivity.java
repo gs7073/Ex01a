a64991002ae4780338e2cd5174dab30ad8fdc1c1
gs7073 <gs7073@bs.amalnet.k12.il>
2025-01-05 21:16:02 +0200

    second commit

--- app/src/main/java/com/example/ex01/MainActivity.java
+++ app/src/main/java/com/example/ex01/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ex01;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +13,27 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        };
+        Button btn;
+        btn = findViewById(R.id.btnMain);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    btn.setText("Oh, yea, I’ve been clicked!\n");
+            }
+
+
+        });
     }
+}
+
+
+
+
+
+
+
